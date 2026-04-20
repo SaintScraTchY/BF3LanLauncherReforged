56dcc8726271cdb72c127527cb9abdf5222feea7
SaintScraTchY <mehrshad2028@gmail.com>
2026-04-20 20:48:22 +0330

    minor directory adjustment

--- BF3LanReforged/Models/CoopMapInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BF3LanReforged;
-
-public class CoopMapInfo(string displayName, string levelPath, string imagePath)
-{
-    public string DisplayName { get; } = displayName;
-    public string LevelPath { get; } = levelPath;
-    public string ImagePath { get; } = imagePath;
-}
--- BF3LanReforged/Models/GameModeInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace BF3LanReforged;
-
-public class GameModeInfo(string displayName, string internalName)
-{
-    public string DisplayName { get; } = displayName;
-    public string InternalName { get; } = internalName;
-}
